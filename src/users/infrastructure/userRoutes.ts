88849acbf3931c9a33fa9e033e03001a6e0ae6b2
NoeOnDev <alxg5516@gmail.com>
2024-09-19 15:36:59 -0600

    Refactorizar userRoutes para agregar ruta getAllUsers

--- src/users/infrastructure/userRoutes.ts
+++ src/users/infrastructure/userRoutes.ts
@@ -4,6 +4,7 @@ import { userController } from "./userDI";
 const router = Router();
 
 router.post("/users", userController.createUser.bind(userController));
+router.get("/users", userController.getAllUsers.bind(userController));
 router.get("/users/:uuid", userController.getUserByUuid.bind(userController));
 router.get(
   "/users/username/:username",
@@ -13,7 +14,6 @@ router.get(
   "/users/email/:email",
   userController.getUserByEmail.bind(userController)
 );
-router.get("/users", userController.getAllUsers.bind(userController));
 router.put("/users/:uuid", userController.updateUser.bind(userController));
 router.patch("/users/:uuid", userController.patchUser.bind(userController));
 router.delete("/users/:uuid", userController.deleteUser.bind(userController));
