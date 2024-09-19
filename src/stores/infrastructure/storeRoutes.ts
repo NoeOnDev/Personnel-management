131c37ce3ca268a9ae24d5fed1762fd41afb3f2b
NoeOnDev <alxg5516@gmail.com>
2024-09-19 13:54:31 -0600

    Refactorizar rutas y métodos para utilizar UUID en lugar de ID en storeRoutes.ts

--- src/stores/infrastructure/storeRoutes.ts
+++ src/stores/infrastructure/storeRoutes.ts
@@ -3,21 +3,20 @@ import { storeController } from "./storeDI";
 
 const router = Router();
 
-router.post("/stores", (req, res) => storeController.createStore(req, res));
-router.get("/stores/:uuid", (req, res) =>
-  storeController.getStoreByUuid(req, res)
+router.post("/stores", storeController.createStore.bind(storeController));
+router.get(
+  "/stores/:uuid",
+  storeController.getStoreByUuid.bind(storeController)
 );
-router.get("/stores/name/:name", (req, res) =>
-  storeController.getStoreByName(req, res)
+router.get(
+  "/stores/name/:name",
+  storeController.getStoreByName.bind(storeController)
 );
-router.put("/stores/:uuid", (req, res) =>
-  storeController.updateStore(req, res)
-);
-router.patch("/stores/:uuid", (req, res) =>
-  storeController.patchStore(req, res)
-);
-router.delete("/stores/:uuid", (req, res) =>
-  storeController.deleteStore(req, res)
+router.put("/stores/:uuid", storeController.updateStore.bind(storeController));
+router.patch("/stores/:uuid", storeController.patchStore.bind(storeController));
+router.delete(
+  "/stores/:uuid",
+  storeController.deleteStore.bind(storeController)
 );
 
 export default router;
